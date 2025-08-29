bd9dac3a3d40e84fcd388a66f793ff989a0c19ed
Thijs Petter <thijspetter@gmail.com>
2025-08-29 11:40:47 +0200

    Shift actorId to UserMessage
    Actor id is always required, so we put it at level of UserMessage

--- engine/src/main/java/org/cafienne/storage/actormodel/message/StorageEvent.scala
+++ engine/src/main/java/org/cafienne/storage/actormodel/message/StorageEvent.scala
@@ -35,7 +35,7 @@ trait StorageEvent extends StorageMessage with CafienneJson with ModelEvent {
   val metadata: ActorMetadata
   val user: StorageUser = metadata.user
   val tenant: String = user.tenant
-  val actorId: String = metadata.actorId
+  override val actorId: String = metadata.actorId
   override val actorType: ActorType = metadata.actorType
   val parentActorId: ActorMetadata = metadata.parent
   val optionalJson: Option[ValueMap] = None
